4eee47084ca9e7a8940a742bd3e4a48452de45ae
Your Name <you@example.com>
2021-05-13 00:35:01 +0100

    version 1.1 spalsh screen animation + new iapp icon

--- app/src/main/java/com/example/notime/SplashScreen.java
+++ app/src/main/java/com/example/notime/SplashScreen.java
@@ -13,11 +13,14 @@ import android.widget.TextView;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.airbnb.lottie.LottieAnimationView;
+
 
 public class SplashScreen extends AppCompatActivity {
 
     ImageView bit_logo;
     TextView no_time_text;
+    LottieAnimationView timer;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -29,12 +32,14 @@ public class SplashScreen extends AppCompatActivity {
 
         bit_logo = findViewById(R.id.bit_logo);
         no_time_text = findViewById(R.id.no_time_text);
+        timer = findViewById(R.id.timer);
 
         Animation text_animation = AnimationUtils.loadAnimation(this, R.anim.text_animation);
         Animation logo_animation = AnimationUtils.loadAnimation(this, R.anim.logo_animation);
 
         no_time_text.setAnimation(text_animation);
         bit_logo.setAnimation(logo_animation);
+        timer.setAnimation(logo_animation);
 
         new Handler().postDelayed(new Runnable() {
             @Override
@@ -44,7 +49,7 @@ public class SplashScreen extends AppCompatActivity {
 //              CustomIntent.customType(SplashScreen.this, "fadein-to-fadeout");
                 finish();
             }
-        },2000);
+        },4000);
 
     }
 }
